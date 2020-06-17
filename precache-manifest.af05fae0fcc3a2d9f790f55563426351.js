8570e2fb059d41dc17a7ad538bf0de535f0b7ddd
Nethra Mugala <nethram@nethras-mbp.attlocal.net>
2020-06-17 15:47:41 -0400

    Updates

---
+++ precache-manifest.af05fae0fcc3a2d9f790f55563426351.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38bd1c809e657d4adb274b9e9f7edb6a",
+    "url": "/react-pokedex/index.html"
+  },
+  {
+    "revision": "1d358afc509db0caad1d",
+    "url": "/react-pokedex/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "17690aea6d0161d85939",
+    "url": "/react-pokedex/static/css/main.382f229f.chunk.css"
+  },
+  {
+    "revision": "1d358afc509db0caad1d",
+    "url": "/react-pokedex/static/js/2.b698bad8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-pokedex/static/js/2.b698bad8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17690aea6d0161d85939",
+    "url": "/react-pokedex/static/js/main.c17aeb4c.chunk.js"
+  },
+  {
+    "revision": "a6f53391d15d59d26871",
+    "url": "/react-pokedex/static/js/runtime-main.0876dcbc.js"
+  }
+]);
